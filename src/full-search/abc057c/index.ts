c31fd84bd45054763a5280c3f0be81c3ec86b02d
suzukalight <mail@mkubara.com>
2021-02-11 14:46:02 +0900

    Add abc057c

---
+++ src/full-search/abc057c/index.ts
@@ -0,0 +1,31 @@
+namespace abc057c {
+  const f = (a: number, b: number) => {
+    const digitsA = `${a}`.length;
+    const digitsB = `${b}`.length;
+    return Math.max(digitsA, digitsB);
+  };
+
+  const solve = (n: number) => {
+    // N<=A*B で取りうる A,B の最大値は sqrt(N)
+    // sqrt(N) まで A,B のペアを全探索
+    const rt = Math.sqrt(n);
+    let minF = Number.MAX_VALUE;
+
+    for (let a = 1; a <= rt; a++) {
+      if (n % a) continue;
+      const b = n / a;
+      minF = Math.min(minF, f(a, b));
+    }
+
+    return minF;
+  };
+
+  const main = (input: string) => {
+    const n = parseInt(input.trim(), 10);
+
+    console.log(solve(n));
+  };
+
+  const input = `9876543210`;
+  main(input);
+}
